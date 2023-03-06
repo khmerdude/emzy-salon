167f672313c2d6b43b3f356ebf99470a8c0d8ece
khmerdude <jett1004@gmail.com>
2023-03-06 09:00:20 -0500

    Folder structure created

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './components/home/Home';
+import Navbar from './components/navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Home />
+    <Navbar />
+    <Switch>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+  </Router>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
