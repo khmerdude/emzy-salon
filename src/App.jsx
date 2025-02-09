f70cdc5f2b76b6d0992189e233106f1951aea2b3
Your Name <your-email@example.com>
2025-02-09 09:45:00 -0500

    Add GitHub Pages routing config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
@@ -9,7 +9,7 @@ function App() {
   const [isSpaModalOpen, setIsSpaModalOpen] = useState(false);
 
   return (
-    <Router>
+    <Router basename="/emzy-salon">
       <div>
         <Navbar 
           openHairModal={() => setIsHairModalOpen(true)}
