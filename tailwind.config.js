a2693b70d82ad3b2d0e3df4c7504f0f76105021c
Your Name <your-email@example.com>
2025-02-09 10:00:58 -0500

    Add Tailwind safelist for dark backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  safelist: [
+    'bg-black',
+    'bg-gray-50',
+    'bg-gray-800',
+    'text-white',
+    'text-gray-400',
+    'text-gray-600'
   ],
   theme: {
     extend: {},
